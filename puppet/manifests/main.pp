3b66259da5f9f6c05f5df9da67b41fb0e5a675dc
Tom Mount <tmountjr@ebay.com>
2014-03-28 14:56:13 -0400

    modified some files for dev environment

--- puppet/manifests/main.pp
+++ puppet/manifests/main.pp
@@ -1,2 +1,28 @@
-# Empty puppet manifest.
-# Add config here to use your modules managed by librarian-puppet.
+Exec { path => ["/bin/", "/sbin/", "/usr/bin/", "/usr/sbin"] }
+
+class { 'apache': }
+
+class { 'php':
+	package => ['php5-cli', 'libapache2-mod-php5', 'php5-mcrypt'],
+}
+
+class { 'mysql::server':
+	root_password => 'root',
+	old_root_password => '',
+	restart => true,
+	service_enabled => true,
+}
+
+mysql::db { 'site_development':
+	user => 'site_dev',
+	password => 'site_development',
+	host => 'localhost',
+	grant => ['all'],
+	require => Class['mysql::server']
+}
+
+php::module { 'mysql':
+	require => [Class['mysql::server'], Class['php']],
+}
+
+# figure out why apache isn't serving php 
---
+++ shell/finish.sh
@@ -0,0 +1,4 @@
+#/usr/bin/env bash
+
+rm -rf /var/www
+ln -fs /vagrant /var/www
